58ec81190c96fd7b533e88b528f8ef351a6f53c4
Yavuzgrdl <32365733+Yavuzgrdl@users.noreply.github.com>
2020-04-21 10:57:06 +0200

    data.js eklendi

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const aboutMeData = {
+    title: 'This is what we can do for you',
+    subTitle: 'Who are in extremely love with eco friendly system.',
+    categories: ['all', 'vector', 'raster', 'ui/ux', 'printing'],
+
+    aboutMeDivBox: [
+        {
+            divClassName: 'bio',
+            h3ClassName: 'text-secondary',
+            h3Title: 'BIO',
+            h6Title: '',
+            pText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium cumque maiores impedit ad vero vitae aliquam dolores voluptatem natus porrosed dolor facilis suscipit eum aliquid quia illo,dolorem repudiandae!'
+        },
+        {
+            divClassName: 'job job-1',
+            h3ClassName: '',
+            h3Title: '123 Webshop',
+            h6Title: 'Lorem, ipsum dolor.',
+            pText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium cumque maiores impedit ad vero vitae aliquam dolores voluptatem natus porro sed.'
+        },
+        {
+            divClassName: 'job job-2',
+            h3ClassName: '',
+            h3Title: 'Designer',
+            h6Title: 'Lorem, ipsum dolor.',
+            pText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium cumque maiores impedit ad vero vitae aliquam dolores voluptatem natus porro sed.'
+        },
+        {
+            divClassName: 'job job-3',
+            h3ClassName: '',
+            h3Title: 'react developer',
+            h6Title: 'Lorem, ipsum dolor.',
+            pText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium cumque maiores impedit ad vero vitae aliquam dolores voluptatem natus porro sed.'
+        }
+
+    ]
+}
