19686047f54d7243955f11c594d4abe7a0644db7
Yavuzgrdl <32365733+Yavuzgrdl@users.noreply.github.com>
2020-05-03 01:28:01 +0200

    Updates

---
+++ precache-manifest.6e211e6e20e7791195da94dc7e89cbf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f971724353970467a48c741dae19a999",
+    "url": "/react_portfolio_responsive/index.html"
+  },
+  {
+    "revision": "4ba5ce4a66472ee9883e",
+    "url": "/react_portfolio_responsive/static/css/main.8f746ce9.chunk.css"
+  },
+  {
+    "revision": "b528f407bcb926427caa",
+    "url": "/react_portfolio_responsive/static/js/2.0b7bccdd.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "/react_portfolio_responsive/static/js/2.0b7bccdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba5ce4a66472ee9883e",
+    "url": "/react_portfolio_responsive/static/js/main.6ec65072.chunk.js"
+  },
+  {
+    "revision": "7b34bb97b02d9abbac56",
+    "url": "/react_portfolio_responsive/static/js/runtime-main.a9b26578.js"
+  }
+]);
